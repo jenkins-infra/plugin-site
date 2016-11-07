01c5dac99f16e6377af520daa878c05dda133559
gusreiber <greiber@cloudbees.com>
2016-11-07 15:31:45 -0800

    [WEBSITE-220] 404 page

--- app/components/NotFound.jsx
+++ app/components/NotFound.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 const NotFound = () =>
-  <div>
-    <h3>404 page not found</h3>
-    <p>We are sorry but the page you are looking for does not exist.</p>
+  <div className="not-found-box">
+    <div className="not-found">
+      <i className="icon-plug"></i>
+      <i className="icon-ban"></i>
+      <h3>Plugin page not found</h3>
+      <p>
+        We are sorry but the page you are looking for does not exist.<br />
+        <a href="/">Search again</a>?
+      </p>
+    </div>
   </div>
-
 export default NotFound;
