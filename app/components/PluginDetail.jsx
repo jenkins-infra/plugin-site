a97e1b781e64d27ec5fa91c65a2945c72ddd2771
Michael McCaskill <mmccaskill@cloudbees.com>
2016-11-08 08:37:03 -0500

    [WEBSITE-220] - Remove Spinner for now
    When shared header is injected it is hidden behind some of the text. I
    need to consult with Gus in order to fix it. To be fair this was out of
    scope with the original ticket anyway.

--- app/components/PluginDetail.jsx
+++ app/components/PluginDetail.jsx
@@ -5,9 +5,6 @@ import { browserHistory, Link } from 'react-router';
 import moment from 'moment';
 import LineChart from './LineChart';
 import NotFound from './NotFound';
-import Spinner from './Spinner';
-import styles from '../styles/Main.css';
-import classNames from 'classnames';
 import { cleanTitle } from '../commons/helper';
 import { firstVisit, isFetchingPlugin, labels, plugin } from '../selectors';
 import { actions } from '../actions';
@@ -123,13 +120,7 @@ class PluginDetail extends React.PureComponent {
     const { isFetchingPlugin, plugin } = this.props;
     if (plugin === null) {
       if (isFetchingPlugin) {
-        return (
-          <div className={classNames(styles.GridBox, 'grid-box')}>
-            <div className={classNames(styles.Grid, 'grid')}>
-              <Spinner />
-            </div>
-          </div>
-        );
+        return null;
       } else {
         return <NotFound/>;
       }
