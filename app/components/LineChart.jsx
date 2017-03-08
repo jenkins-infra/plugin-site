4c56d41290780d6b06f581f10b7801a2fa74adcc
Michael McCaskill <mmccaskill@cloudbees.com>
2017-03-08 13:02:03 -0500

    [WEBSITE-240] - Use different point color
    Also took it as an opportunity to upgrade the chart library as chart.js
    is 2.x now.

--- app/components/LineChart.jsx
+++ app/components/LineChart.jsx
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import { Line } from 'react-chartjs';
+import { Line } from 'react-chartjs-2';
 import moment from 'moment';
 
 const calculateHeight = (total) => {
@@ -11,7 +11,7 @@ const calculateHeight = (total) => {
   return 150;
 };
 
-const chartData = (labels, data) => {
+const calculateMinMax = (data) => {
   const lastValue = data[data.length-1];
   let maxValue = null;
   let minValue = null;
@@ -25,52 +25,58 @@ const chartData = (labels, data) => {
   if (lastValue > 5000) minValue = 0;
 
   return {
+    min: minValue,
+    max: maxValue
+  };
+}
+
+const chartData = (labels, data) => {
+  return {
     labels: labels,
     datasets: [
       {
         label: 'Installs',
-        fillColor: 'rgba(0,220,255,0.3)',
-        strokeColor: 'rgba(220,220,220,1)',
-        pointColor: 'rgba(220,220,220,1)',
-        pointStrokeColor: '#fff',
-        pointHighlightFill: '#fff',
-        pointHighlightStroke: 'rgba(220,220,220,1)',
         data: data,
-      },
-      {
-        strokeColor: 'rgba(0,0,0,0)',
-        pointColor: 'rgba(0,0,0,0)',
-        pointStrokeColor: 'rgba(0,0,0,0)',
-        label: '',
-        data: [minValue]
-      },
-      {
-        label: '',
-        data: [maxValue]
+        backgroundColor: 'rgba(0,220,255,0.3)',
+        pointBackgroundColor: 'rgb(51, 153, 12)',
+        pointRadius: 2,
+        pointHitRadius: 10,
       }
     ]
   };
 };
 
-const options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  scaleShowGridLines: true,
-  scaleGridLineColor: 'rgba(0,0,0,.05)',
-  scaleGridLineWidth: 1,
-  scaleFontSize: 9,
-  scaleShowHorizontalLines: true,
-  scaleShowVerticalLines: true,
-  bezierCurve: true,
-  bezierCurveTension: 0.4,
-  pointDot: true,
-  pointDotRadius: 2,
-  pointDotStrokeWidth: 1,
-  pointHitDetectionRadius: 10,
-  datasetStroke: true,
-  datasetStrokeWidth: 2,
-  datasetFill: true,
-  legendTemplate: '<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>', // eslint-disable-line max-len
+const options = (data) => {
+  const { min, max } = calculateMinMax(data)
+  return {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      display: false
+    },
+    elements: {
+      line: {
+        borderWidth: 2,
+      },
+      point: {
+        borderWidth: 1
+      }
+    },
+    scales: {
+      xAxes: [{
+        ticks: {
+          fontSize: 9,
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min,
+          max,
+          fontSize: 9,
+        }
+      }]
+    }
+  };
 };
 
 const styles = {
@@ -110,7 +116,7 @@ export default class LineChart extends React.PureComponent {
     return (
       <div style={styles.graphContainer}>
         <Line data={lineData}
-          options={options}
+          options={options(data)}
           height={height}
           redraw />
       </div>
