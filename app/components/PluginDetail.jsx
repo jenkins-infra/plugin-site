f9bb529d4bcd1c519436c71b08e639a4e8ace705
Jesse Glick <jglick@cloudbees.com>
2019-09-11 09:47:07 -0400

    Use ≥ rather than v. notation.

--- app/components/PluginDetail.jsx
+++ app/components/PluginDetail.jsx
@@ -123,7 +123,7 @@ class PluginDetail extends React.PureComponent {
       }
       return (
         <div key={dependency.name} className={kind}>
-          <Link to={`/${dependency.name}`}>{dependency.title} v.{dependency.version} <span className="req">({kind})</span></Link>
+          <Link to={`/${dependency.name}`}>{dependency.title} ≥ {dependency.version} <span className="req">({kind})</span></Link>
         </div>
       );
     });
