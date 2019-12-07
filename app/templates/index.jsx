dd96b79105b929d7595a749a75efff56c0b79385
Gavin Mogan <git@gavinmogan.com>
2019-12-07 01:12:10 -0800

    Oops, forgot to copy over all the functional items to make the index page work

--- app/templates/index.jsx
+++ app/templates/index.jsx
@@ -1,38 +1,45 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import styles from '../styles/Main.css';
 
 import Footer from '../components/Footer';
 
-function IndexPage({ }) {
-  return (
-    <div>
-      <div className={classNames(styles.ItemFinder, this.props.view, { showResults: this.props.showResults },
-        { isFiltered: this.props.isFiltered }, 'item-finder')}>
-        <form action="#" id="plugin-search-form"
-          className={classNames(styles.HomeHeader, { showFilter: this.props.showFilter }, 'HomeHeader jumbotron')}
-          onSubmit={this.handleOnSubmit}>
-          <h1>Plugins Index</h1>
-          <p>
+function IndexPage({isFiltered, showResults, showFilter, view}) {
+    const handleOnSubmit = (e) => {};
+
+    return (
+        <div>
+            <div className={classNames(styles.ItemFinder, view, {showResults: showResults},
+                {isFiltered: isFiltered}, 'item-finder')}>
+                <form action="#" id="plugin-search-form"
+                    className={classNames(styles.HomeHeader, {showFilter: showFilter}, 'HomeHeader jumbotron')}
+                    onSubmit={handleOnSubmit}>
+                    <h1>Plugins Index</h1>
+                    <p>
             Discover the 1500+ community contributed Jenkins plugins to support building, deploying and automating any project.
-          </p>
-          <nav className={classNames(styles.navbar, 'navbar')}>
-            <div className="nav navbar-nav">
-              {/*
-              <SearchBox handleOnSubmit={this.handleOnSubmit} />
-              <Views />
-              */}
+                    </p>
+                    <nav className={classNames(styles.navbar, 'navbar')}>
+                        <div className="nav navbar-nav">
+                            {/* <SearchBox handleOnSubmit={handleOnSubmit} /> */}
+                            {/* <Views /> */}
+                        </div>
+                    </nav>
+                    {/* <Filters /> */}
+                </form>
+                {/* <SearchResults /> */}
+                <Footer />
             </div>
-          </nav>
-          {/* <Filters /> */}
-        </form>
-        {/* <SearchResults /> */}
-        <Footer />
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
+
 IndexPage.propTypes = {
+    isFiltered: PropTypes.bool,
+    showResults: PropTypes.bool,
+    showFilter: PropTypes.bool,
+    view: PropTypes.object
 };
+
 
 export default IndexPage;
