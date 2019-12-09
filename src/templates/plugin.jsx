52ff130bfc205fd959eab67329ae88c3113a2b2a
Gavin Mogan <git@gavinmogan.com>
2019-12-08 16:15:19 -0800

    re-add the dialog component

--- src/templates/plugin.jsx
+++ src/templates/plugin.jsx
@@ -13,6 +13,7 @@ import PluginActiveWarnings from '../components/PluginActiveWarnings';
 import PluginInactiveWarnings from '../components/PluginInactiveWarnings';
 import PluginMaintainers from '../components/PluginMaintainers';
 import PluginReadableInstalls from '../components/PluginReadableInstalls';
+import Dialog from '../components/Dialog';
 
 function shouldShowWikiUrl({url}) {
     return url && (url.startsWith('https://wiki.jenkins-ci.org') || url.startsWith('https://wiki.jenkins.io'));
@@ -31,77 +32,104 @@ function PluginPage({data: {jenkinsPlugin: plugin}}) {
     return (
         <Layout>
             <SEO title={cleanTitle(plugin.title)} description={plugin.excerpt} pathname={`/${plugin.id}`}/>
-            <section className="modalview">
-                <div className="dialog">
-                    <div className="row flex" style={{margin: 0}}>
-                        <div className="col-md-9 main">
-                            <div className="container-fluid padded">
-                                <h1 className="title">
-                                    {cleanTitle(plugin.title)}
-                                    <PluginActiveWarnings securityWarnings={plugin.securityWarnings} />
-                                    <span className="v">{plugin.version}</span>
-                                    <span className="sub">Minimum Jenkins requirement: {plugin.requiredCore}</span>
-                                    <span className="sub">ID: {plugin.name}</span>
-                                </h1>
-                                <div className="row flex">
-                                    <div className="col-md-4">
-                                        {plugin.stats && <div>Installs: <PluginReadableInstalls currentInstalls={plugin.stats.currentInstalls} /></div>}
-                                        {plugin.scm && plugin.scm.link && <div><a href={plugin.scm.link}>GitHub →</a></div>}
-                                        <PluginLastReleased plugin={plugin} />
-                                    </div>
-                                    <div className="col-md-4 maintainers">
-                                        <h5>Maintainers</h5>
-                                        <PluginMaintainers maintainers={plugin.maintainers} />
-                                    </div>
-                                    <div className="col-md-4 dependencies">
-                                        <h5>Dependencies</h5>
-                                        <PluginDependencies dependencies={plugin.dependencies} />
-                                    </div>
+            <Dialog title="Find Plugins">
+                <div className="row flex" style={{margin: 0}}>
+                    <div className="col-md-9 main">
+                        <div className="container-fluid padded">
+                            <h1 className="title">
+                                {cleanTitle(plugin.title)}
+                                <PluginActiveWarnings securityWarnings={plugin.securityWarnings} />
+                                <span className="v">{plugin.version}</span>
+                                <span className="sub">
+Minimum Jenkins requirement:
+                                    {' '}
+                                    {plugin.requiredCore}
+                                </span>
+                                <span className="sub">
+ID:
+                                    {' '}
+                                    {plugin.name}
+                                </span>
+                            </h1>
+                            <div className="row flex">
+                                <div className="col-md-4">
+                                    {plugin.stats && <div>
+Installs:
+                                        {' '}
+                                        <PluginReadableInstalls currentInstalls={plugin.stats.currentInstalls} />
+                                    </div>}
+                                    {plugin.scm && plugin.scm.link && <div><a href={plugin.scm.link}>GitHub →</a></div>}
+                                    <PluginLastReleased plugin={plugin} />
                                 </div>
-                                {plugin.wiki.content && <div className="content" dangerouslySetInnerHTML={{__html: plugin.wiki.content}} />}
+                                <div className="col-md-4 maintainers">
+                                    <h5>Maintainers</h5>
+                                    <PluginMaintainers maintainers={plugin.maintainers} />
+                                </div>
+                                <div className="col-md-4 dependencies">
+                                    <h5>Dependencies</h5>
+                                    <PluginDependencies dependencies={plugin.dependencies} />
+                                </div>
                             </div>
-                        </div>
-                        <div className="col-md-3 gutter">
-                            <a href={`https://updates.jenkins.io/download/plugins/${plugin.name}`}
-                                className="btn btn-secondary">
-                                <i className="icon-box" />
-                                <span>Archives</span>
-                                <span className="v">Get past versions</span>
-                            </a>
-                            <div className="chart">
-                                <LineChart
-                                    total={plugin.stats.currentInstalls}
-                                    installations={plugin.stats.installations}
-                                />
-                            </div>
-                    
-                            <h5>Links</h5>
-                            {plugin.scm && plugin.scm.link && <div><a href={plugin.scm.link}>GitHub</a></div>}
-                            <div><a href={`https://javadoc.jenkins.io/plugin/${plugin.name}`}>Javadoc</a></div>
-                    
-                            <h5>Labels</h5>
-                            <PluginLabels labels={plugin.labels} />
-                            <br/>
-                            {shouldShowWikiUrl(plugin.wiki) &&
-                                <div className="update-link">
-                                    <h5>Help us to improve this page!</h5>
-                        This content is served from the <a href={plugin.wiki.url} target="_wiki">Jenkins Wiki</a> which is in the <a href="https://groups.google.com/forum/#!msg/jenkinsci-dev/lNmas8aBRrI/eL3u7A6qBwAJ" target="_blank">read-only state</a>.
-                        We recommend moving the plugin documentation to GitHub, see the guidelines <a href="https://jenkins.io/blog/2019/10/21/plugin-docs-on-github/" target="_blank">here</a>.
-                                </div>
-                            }
-                            {shouldShowGitHubUrl(plugin.wiki) &&
-                                <div className="update-link">
-                                    <h5>Help us to improve this page!</h5>
-                        To propose a change submit a pull request to <a href={plugin.wiki.url} target="_blank">the plugin page</a> on GitHub.
-                        Read more about GitHub support on the plugin site in the <a href="https://jenkins.io/doc/developer/publishing/documentation/" target="_blank">Jenkins developer documentation</a>.  
-                                </div>
-                            }
-        
-                            <PluginInactiveWarnings securityWarnings={plugin.securityWarnings} />
+                            {plugin.wiki.content && <div className="content" dangerouslySetInnerHTML={{__html: plugin.wiki.content}} />}
                         </div>
                     </div>
+                    <div className="col-md-3 gutter">
+                        <a href={`https://updates.jenkins.io/download/plugins/${plugin.name}`}
+                            className="btn btn-secondary">
+                            <i className="icon-box" />
+                            <span>Archives</span>
+                            <span className="v">Get past versions</span>
+                        </a>
+                        <div className="chart">
+                            <LineChart
+                                total={plugin.stats.currentInstalls}
+                                installations={plugin.stats.installations}
+                            />
+                        </div>
+                    
+                        <h5>Links</h5>
+                        {plugin.scm && plugin.scm.link && <div><a href={plugin.scm.link}>GitHub</a></div>}
+                        <div><a href={`https://javadoc.jenkins.io/plugin/${plugin.name}`}>Javadoc</a></div>
+                    
+                        <h5>Labels</h5>
+                        <PluginLabels labels={plugin.labels} />
+                        <br/>
+                        {shouldShowWikiUrl(plugin.wiki) &&
+                            <div className="update-link">
+                                <h5>Help us to improve this page!</h5>
+                        This content is served from the 
+                                {' '}
+                                <a href={plugin.wiki.url} target="_wiki">Jenkins Wiki</a>
+                                {' '}
+which is in the
+                                {' '}
+                                <a href="https://groups.google.com/forum/#!msg/jenkinsci-dev/lNmas8aBRrI/eL3u7A6qBwAJ" target="_blank">read-only state</a>
+.
+                        We recommend moving the plugin documentation to GitHub, see the guidelines
+                                {' '}
+                                <a href="https://jenkins.io/blog/2019/10/21/plugin-docs-on-github/" target="_blank">here</a>
+.
+                            </div>
+                        }
+                        {shouldShowGitHubUrl(plugin.wiki) &&
+                            <div className="update-link">
+                                <h5>Help us to improve this page!</h5>
+                        To propose a change submit a pull request to 
+                                {' '}
+                                <a href={plugin.wiki.url} target="_blank">the plugin page</a>
+                                {' '}
+on GitHub.
+                        Read more about GitHub support on the plugin site in the
+                                {' '}
+                                <a href="https://jenkins.io/doc/developer/publishing/documentation/" target="_blank">Jenkins developer documentation</a>
+.  
+                            </div>
+                        }
+        
+                        <PluginInactiveWarnings securityWarnings={plugin.securityWarnings} />
+                    </div>
                 </div>
-            </section>
+            </Dialog>
         </Layout>
     );
 }
