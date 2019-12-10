8f71e59d3ed0a18738945a011a6fb3414204495f
Gavin Mogan <git@gavinmogan.com>
2019-12-10 12:53:51 -0800

    move .eslintrc to .eslintrc.js so we can run rules on itself

---
+++ .eslintrc.js
@@ -0,0 +1,78 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:import/recommended',
+        'plugin:react/recommended'
+    ],
+    'overrides': [
+        {
+            'env': {'jest': true},
+            'extends': ['plugin:jest/recommended'],
+            'files': ['__tests__/**/*.js'],
+            'plugins': ['jest'],
+            'rules': {}
+        }
+    ],
+    'parser': 'babel-eslint',
+    'plugins': [
+        'react',
+        'import'
+    ],
+    'rules': {
+        'eol-last': 2,
+        'import/extensions': ['error', 'ignorePackages', {'js': 'never', 'jsx': 'never'}],
+        'import/no-unresolved': [2],
+        'indent': ['error', 4],
+        'jsx-quotes': 2,
+        'key-spacing': [2],
+        'max-len': 0,
+        'no-console': 2,
+        'no-extra-semi': 2,
+        'no-multi-spaces': 'error',
+        'no-trailing-spaces': [0, {'skipBlankLines': true}],
+        'no-undef': 2,
+        'no-underscore-dangle': [0],
+        'no-unused-vars': [2],
+        'no-var': 2,
+        'object-curly-spacing': ['error', 'never'],
+        'object-shorthand': [0, 'always'],
+        'prefer-arrow-callback': 2,
+        'prefer-const': 2,
+        'prefer-template': 2,
+        'quote-props': [0, 'as-needed'],
+        'quotes': [2, 'single'],
+        'react/display-name': 0,
+        'react/jsx-boolean-value': 2,
+        'react/jsx-curly-spacing': [2, {'allowMultiline': true, 'when': 'never'}],
+        'react/jsx-equals-spacing': [2, 'never'],
+        'react/jsx-filename-extension': [1, {'extensions': ['.js', '.jsx']}],
+        'react/jsx-indent': ['error', 4, {'checkAttributes': true, 'indentLogicalExpressions': true}],
+        'react/jsx-no-undef': 2,
+        'react/jsx-one-expression-per-line': ['error', {'allow': 'single-child'}],
+        'react/jsx-sort-props': 0,
+        'react/jsx-uses-react': 2,
+        'react/jsx-uses-vars': 2,
+        'react/jsx-wrap-multilines': 2,
+        'react/no-did-mount-set-state': 2,
+        'react/no-did-update-set-state': 2,
+        'react/no-multi-comp': 0,
+        'react/no-unknown-property': 2,
+        'react/prop-types': 2,
+        'react/react-in-jsx-scope': 2,
+        'react/self-closing-comp': 2,
+        'semi': [2],
+        'sort-keys': ['error', 'asc', {'caseSensitive': true, 'minKeys': 2, 'natural': false}],
+        'space-before-function-paren': [2, {'anonymous': 'always','named': 'never'}],
+        'strict': [2, 'global'],
+    },
+    'settings': {
+        'import/extensions': [ '.js', '.jsx' ],
+        'import/resolver': {'node': {'extensions': ['.js', '.jsx']}},
+        'react': {'version': 'detect'},
+    },
+};
