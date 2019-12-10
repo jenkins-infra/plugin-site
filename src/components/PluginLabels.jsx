ef651c4e97f70815b7e6c2aff73104017a48968a
Gavin Mogan <git@gavinmogan.com>
2019-12-10 01:01:23 -0800

    PluginLabels should show the pretty version of label names, not the id

--- src/components/PluginLabels.jsx
+++ src/components/PluginLabels.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from 'gatsby';
+import {Link, useStaticQuery, graphql} from 'gatsby';
 
 function PluginLabels({labels}) {
+    const allLabels = useStaticQuery(graphql`
+        query {
+            labels: allJenkinsPluginLabel {
+                edges {
+                    node {
+                        id
+                        title
+                    }
+                }
+            }
+        }
+    `).labels.edges.reduce((prev, {node: label}) => {
+        prev[label.id] = label.title;
+        return prev;
+    }, {});
+
     if (!labels || labels.length === 0) {
         return (<div className="empty">This plugin has no labels</div>);
     }
     return labels.map((id) => {
-        const label = labels.find((label) => label.id === id);
-        const text = label !== undefined ? label.title : id;
+        const text = (allLabels[id] || id).replace(' development', '');
+        // FIXME - search page
         return (
             <div className="label-link" key={id}>
                 <Link to={`/?labels=${id}`}>{text}</Link>
