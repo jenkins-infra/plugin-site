105cdd558b9d2134933fbbbf98d071f0dcb94d24
Gavin Mogan <git@gavinmogan.com>
2020-01-06 19:57:03 -0800

    Make front page all css grid instead of hacking prepend css so it can be layed out better for mobile

--- src/templates/index.jsx
+++ src/templates/index.jsx
@@ -22,13 +22,16 @@ function IndexPage() {
         <Layout>
             <SEO />
             <div className="IndexPage--Container jumbotron" onSubmit={handleOnSubmit}>
-                <h1>Plugins Index</h1>
-                <p>Discover the 1500+ community contributed Jenkins plugins to support building, deploying and automating any project.</p>
-                <SearchBox 
-                    handleOnSubmit={handleOnSubmit}
-                    query={query}
-                    setQuery={setQuery}
-                />
+                <div className="logo" />
+                <div className="content">
+                    <h1>Plugins Index</h1>
+                    <p>Discover the 1500+ community contributed Jenkins plugins to support building, deploying and automating any project.</p>
+                    <SearchBox 
+                        handleOnSubmit={handleOnSubmit}
+                        query={query}
+                        setQuery={setQuery}
+                    />
+                </div>
             </div>
             <Footer />
         </Layout>
