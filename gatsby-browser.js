fba4f09e8bb310f32f70a31c677143a08bcb16c1
Gavin Mogan <git@gavinmogan.com>
2020-01-20 18:10:56 -0800

    querySelectorAll needs to be wrapped in Array.from for foreach to function in old browsers

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,7 @@
 exports.onInitialClientRender = () => {
     // ugly fallback, wait 250ms, and if any css that didn't preload (firefox), tell it to load
     setTimeout(() => {
-        document.querySelectorAll('link[rel="preload"]').forEach(elm => {
+        Array.from(document.querySelectorAll('link[rel="preload"]')).forEach(elm => {
             elm.onload=null; elm.rel='stylesheet';
         });
     }, 250);
