bda6fe90d7d16f3f502e4bf59d9619c177638684
Gavin Mogan <git@gavinmogan.com>
2020-03-10 00:35:37 -0700

    importing the sentry ignore list from sentry website

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,11 +116,49 @@ module.exports = {
                 dsn: 'https://495b0bd32a5e4a2287c3fe4b061ee24f@sentry.io/1882460',
                 environment: process.env.NODE_ENV,
                 enabled: ['production', 'stage'].includes(process.env.NODE_ENV),
-                blacklistUrls: [
-                    'regex:^chrome-extension:',
-                    'regex:^moz-extension:',
-                    'regex:should_do_lastpass_here',
+                // from https://docs.sentry.io/platforms/javascript/#decluttering-sentry
+                ignoreErrors: [
+                    // Random plugins/extensions
+                    'top.GLOBALS',
+                    // See: http://blog.errorception.com/2012/03/tale-of-unfindable-js-error.html
+                    'originalCreateNotification',
+                    'canvas.contentDocument',
+                    'MyApp_RemoveAllHighlights',
+                    'http://tt.epicplay.com',
+                    'Can\'t find variable: ZiteReader',
+                    'jigsaw is not defined',
+                    'ComboSearch is not defined',
+                    'http://loading.retry.widdit.com/',
+                    'atomicFindClose',
+                    // Facebook borked
+                    'fb_xd_fragment',
+                    // ISP "optimizing" proxy - `Cache-Control: no-transform` seems to
+                    // reduce this. (thanks @acdha)
+                    // See http://stackoverflow.com/questions/4113268
+                    'bmi_SafeAddOnload',
+                    'EBCallBackMessageReceived',
+                    // See http://toolbar.conduit.com/Developer/HtmlAndGadget/Methods/JSInjection.aspx
+                    'conduitPage',
                     'Cannot redefine property: BetterJsPop',
+                    'should_do_lastpass_here',
+                ],
+                blacklistUrls: [
+                    '^chrome-extension:',
+                    '^moz-extension:',
+                    // Facebook flakiness
+                    /graph\.facebook\.com/i,
+                    // Facebook blocked
+                    /connect\.facebook\.net\/en_US\/all\.js/i,
+                    // Woopra flakiness
+                    /eatdifferent\.com\.woopra-ns\.com/i,
+                    /static\.woopra\.com\/js\/woopra\.js/i,
+                    // Chrome extensions
+                    /extensions\//i,
+                    /^chrome:\/\//i,
+                    // Other plugins
+                    /127\.0\.0\.1:4001\/isrunning/i, // Cacaoweb
+                    /webappstoolbarba\.texthelp\.com\//i,
+                    /metrics\.itunes\.apple\.com\.edgesuite\.net\//i
                 ]
             }
         }
