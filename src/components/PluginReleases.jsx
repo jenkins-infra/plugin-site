796c7e326239a6875a132cc090a486f10c41471e
Gavin Mogan <git@gavinmogan.com>
2020-06-01 22:01:50 -0700

    full sized releases cards with nice header and stuff

--- src/components/PluginReleases.jsx
+++ src/components/PluginReleases.jsx
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import ReactMarkdown from 'react-markdown';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import ReactTimeAgo from 'react-time-ago/tooltip';
@@ -30,17 +29,21 @@ function PluginIssues({pluginId}) {
         <div id="pluginReleases--container" className="container">
             {releases && releases.map(release => {
                 return (
-                    <div key={release.tag_name} className="card">
+                    <div key={release.tag_name} className="item card">
+                        <div className="card-header">
+                            <h5 className="card-title d-flex justify-content-between">
+                                <div>{release.name || release.tag_name}</div>
+                                <div>
+                                    {'Released: '}
+                                    <ReactTimeAgo date={new Date(release.published_at)} />
+                                </div>
+                            </h5>
+                        </div>
                         <div className="card-body">
-                            <h5 className="card-title">{release.name || release.tag_name}</h5>
-                            <div>
-                                {'Released: '}
-                                <ReactTimeAgo date={new Date(release.published_at)} />
-                            </div>
-                            <br />
-                            <p className="card-text">
-                                <ReactMarkdown source={release.body.replace('<!-- Optional: add a release summary here -->', '')} />
-                            </p>
+                            <p 
+                                className="card-text"
+                                dangerouslySetInnerHTML={{__html: release.bodyHTML}}
+                            />
                         </div>
                     </div>
                 );
