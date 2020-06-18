1007d4310289b4b85342d07b0750868586c852bd
contactsaurabh <44664783+contactsaurabh@users.noreply.github.com>
2020-06-18 13:26:09 +0530

    Fix url generated for report a problem bug templates (#275)
    Co-authored-by: Saurabh Yadav <saurabhyadav@in.ibm.com>

--- src/components/ReportAProblem.jsx
+++ src/components/ReportAProblem.jsx
@@ -6,7 +6,7 @@ function ReportAProblem({reportProblemTitle, reportProblemUrl, reportProblemRela
     
     return (
         <p className="box">
-            <a href={`https://github.com/jenkins-infra/plugin-site/issues/new?labels=bug&template=4-bug.md&title=${reportProblemTitle} page - TODO: Put a summary here&body=Problem with the [${reportProblemTitle}](https://plugins.jenkins.io${reportProblemUrl}) page, [source file](https://github.com/jenkins-infra/plugin-site/tree/master/src${reportProblemRelativeSourcePath})%0A%0ATODO: Describe the expected and actual behavior here %0A%0A%23%23 Screenshots %0A%0A TODO: Add screenshots if possible %0A%0A%23%23 Possible Solution %0A%0A%3C!-- If you have suggestions on a fix for the bug, please describe it here. --%3E %0A%0AN/A`} title={title}>
+            <a href={`https://github.com/jenkins-infra/plugin-site/issues/new?labels=bug&template=4-bug.md&title=${reportProblemTitle} page - TODO: Put a summary here&body=Problem with the [${reportProblemTitle}](https://plugins.jenkins.io${reportProblemUrl}) page, [source file](https://github.com/jenkins-infra/plugin-site/tree/master/src/${reportProblemRelativeSourcePath})%0A%0ATODO: Describe the expected and actual behavior here %0A%0A%23%23 Screenshots %0A%0A TODO: Add screenshots if possible %0A%0A%23%23 Possible Solution %0A%0A%3C!-- If you have suggestions on a fix for the bug, please describe it here. --%3E %0A%0AN/A`} title={title}>
                 Report a problem
             </a>
         </p>
