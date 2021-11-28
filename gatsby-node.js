f7f07f51817a8e8cc2cb19b8a4112b9edbcb0b88
Gavin Mogan <git@gavinmogan.com>
2021-11-28 14:59:24 -0800

    Enable eslint-promise since its already installed
    * Switch mkdirp to fs.promise.mkdir with recursive
    * use promise fs write instead of sync version

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,28 +1,81 @@
 /* eslint-env node */
 /* eslint-disable no-console */
 const path = require('path');
-const fs = require('fs');
-const mkdirp = require('mkdirp');
+const fs = require('fs/promises');
 
 const {makeReactLayout} = require('./utils');
 
 exports.onPreBootstrap = async () => {
     const {jsxLines, cssLines, manifest} = await makeReactLayout();
     if (manifest) {
-        mkdirp.sync('static');
-        fs.writeFileSync('./static/site.webmanifest', manifest);
+        await fs.mkdir('static', {recursive: true});
+        await fs.writeFile('./static/site.webmanifest', manifest);
     }
     if (jsxLines) {
-        fs.writeFileSync('./src/layout.jsx', jsxLines);
+        await fs.writeFile('./src/layout.jsx', jsxLines);
     }
     if (cssLines) {
-        fs.writeFileSync('./src/layout.css', cssLines);
+        await fs.writeFile('./src/layout.css', cssLines);
     }
 };
 
-exports.createPages = async ({graphql, actions: {createPage}}) => {
+
+async function createPluginPages({graphql, createPage}) {
     const pluginPage = path.resolve('src/templates/plugin.jsx');
+    const result = await graphql(`{
+      allJenkinsPlugin {
+        edges {
+          node {
+            id
+            name
+          }
+        }
+      }
+    }`);
+    if (result.errors) {
+        console.log(result.errors);
+        throw result.errors;
+    }
+
+    result.data.allJenkinsPlugin.edges.forEach(edge => {
+        createPage({
+            path: `/${edge.node.name.trim()}/`,
+            component: pluginPage,
+            context: {
+                name: edge.node.name.trim()
+            }
+        });
+    });
+}
+
+async function createSuspendedPluginPages({graphql, createPage}) {
     const tombstonePage = path.resolve('src/templates/tombstone.jsx');
+    const result = await graphql(`{
+      allSuspendedPlugin {
+        edges {
+          node {
+            id
+            url
+          }
+        }
+      }
+    }`);
+    if (result.errors) {
+        console.log(result.errors);
+        throw result.errors;
+    }
+
+    result.data.allSuspendedPlugin.edges.forEach(edge => {
+        createPage({
+            path: `/${edge.node.id.trim()}/`,
+            component: tombstonePage,
+            context: {
+                name: edge.node.id.trim()
+            }
+        });
+    });
+}
+exports.createPages = async ({graphql, actions: {createPage}}) => {
     const indexPage = path.resolve('src/templates/index.jsx');
     const searchPage = path.resolve('src/templates/search.jsx');
 
@@ -38,56 +91,9 @@ exports.createPages = async ({graphql, actions: {createPage}}) => {
         context: { }
     });
 
-    await graphql(`{
-      allJenkinsPlugin {
-        edges {
-          node {
-            id
-            name
-          }
-        }
-      }
-    }`).then(result => {
-        if (result.errors) {
-            console.log(result.errors);
-            throw result.errors;
-        }
-
-        result.data.allJenkinsPlugin.edges.forEach(edge => {
-            createPage({
-                path: `/${edge.node.name.trim()}/`,
-                component: pluginPage,
-                context: {
-                    name: edge.node.name.trim()
-                }
-            });
-        });
-    });
-
-    await graphql(`{
-      allSuspendedPlugin {
-        edges {
-          node {
-            id
-            url
-          }
-        }
-      }
-    }`).then(result => {
-        if (result.errors) {
-            console.log(result.errors);
-            throw result.errors;
-        }
-
-        result.data.allSuspendedPlugin.edges.forEach(edge => {
-            createPage({
-                path: `/${edge.node.id.trim()}/`,
-                component: tombstonePage,
-                context: {
-                    name: edge.node.id.trim()
-                }
-            });
-        });
-    });
+    await Promise.all([
+        createPluginPages({graphql, createPage}),
+        createSuspendedPluginPages({graphql, createPage}),
+    ]);
 };
 
