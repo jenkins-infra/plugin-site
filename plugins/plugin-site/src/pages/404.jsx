544f0859846539e97ddbeed4ff5d2825154023bf
Gavin Mogan <git@gavinmogan.com>
2022-03-17 15:54:19 -0700

    handle location being undefined (since it will be on server side render)

--- plugins/plugin-site/src/pages/404.jsx
+++ plugins/plugin-site/src/pages/404.jsx
@@ -7,8 +7,13 @@ import PropTypes from 'prop-types';
 const title = 'Plugin page not found';
 const notFoundPage = 'pages/404.jsx';
 
-const NotFound = ({location}) => (
-    <Layout reportProblemRelativeSourcePath={notFoundPage} reportProblemUrl="" reportProblemTitle={title}>
+const NotFound = ({location}) => {
+    let searchQuery = '';
+    if (location && location.href) {
+        searchQuery = location.href.split('/').filter(Boolean).reverse()[0];
+    }
+
+    return (<Layout reportProblemRelativeSourcePath={notFoundPage} reportProblemUrl="" reportProblemTitle={title}>
         <Helmet><title>{title}</title></Helmet>
         <div className="not-found-box">
             <div className="not-found">
@@ -18,17 +23,17 @@ const NotFound = ({location}) => (
                 <p>
                     We are sorry but the page you are looking for does not exist.
                     <br />
-                    <Link to={`/ui/search?query=${location.href.split('/').filter(Boolean).reverse()[0]}`} >Search again</Link>
+                    <Link to={`/ui/search?query=${searchQuery}`} >Search again</Link>
                     ?
                 </p>
             </div>
         </div>
-    </Layout>
-);
+    </Layout>);
+};
 
 NotFound.propTypes = {
     location: PropTypes.shape({
         href: PropTypes.string.isRequired,
-    }).isRequired
+    })
 };
 export default NotFound;
