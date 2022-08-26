b795e4a71bb8718749285b626b3aff9edfe1308c
Zbynek Konecny <zbynek1729@gmail.com>
2022-08-26 07:59:38 +0200

    More robust plugin version comparison

--- plugins/gatsby-jenkinsci-fieldextensions/gatsby-node.js
+++ plugins/gatsby-jenkinsci-fieldextensions/gatsby-node.js
@@ -27,14 +27,18 @@ exports.createSchemaCustomization = ({actions}) => {
             const padArrayEnd = (arr, len, padding) => {
                 return arr.concat(Array(Math.max(len - arr.length, 0)).fill(padding));
             };
+            // qualifiers alphabetically sorted: a(lpha) < b(eta) < r(c) < s(table)
+            const getQualifier = val => val.toString().match(/^(rc|a\d|b\d)/) ? val.charAt(0) : 's';
             return {
                 resolve(source) {
                     const value = source[options.field || 'version'].toString() || '';
                     // make sure the version has 3 parts and 5 length (just in case)
                     // so 1.2.3 and 1.2 sort right
-                    // 2.29 => 00002_00029_00000
-                    // 2.290 => 00002_00290_00000
-                    return padArrayEnd(value.split('.'), 4, 0).map(val => val.toString().padStart(5, '0')).join('_');
+                    // 2.29 => s00002_s00029_s00000
+                    // 2.290 => s00002_s00290_s00000
+                    // 2.290-rc4 => s00002_s00290_r00004
+                    return padArrayEnd(value.split(/[.-]/), 4, 0).map(val => getQualifier(val)
+                                + val.toString().replace(/^rc/, '').replace(/^[ab](\d)/, '$1').padStart(5, '0')).join('_');
                 },
             };
         },
