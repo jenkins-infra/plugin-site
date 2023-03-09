88d1618f99c1019268664e0ba1efd8f2d596dc11
Harsh <harshgupta3341@gmail.com>
2023-03-10 01:41:14 +0530

    made first tab as active if no tab is selected

--- plugins/plugin-site/src/components/PluginPageTabs.jsx
+++ plugins/plugin-site/src/components/PluginPageTabs.jsx
@@ -3,13 +3,26 @@ import PropTypes from 'prop-types';
 
 import {Link} from 'gatsby';
 
-const PluginPageTabs = ({tabs}) => {
+const PluginPageTabs = ({ tabs }) => {
+
+    let isAnyTabActive = false;
+    tabs.forEach((tab) => {
+        if (window.location.pathname === tab.to) {
+            isAnyTabActive = true;
+        }
+    });
+
     return (
         <ul className="nav nav-pills">
-            {tabs.map(tab => {
+            {tabs.map((tab, index) => {
                 return (
                     <li className="nav-item" key={tab.id}>
-                        <Link activeClassName="active" className="nav-link" to={tab.to}>{tab.label}</Link>
+                        <Link
+                            activeClassName="active"
+                            className={`nav-link ${!isAnyTabActive && index === 0 ? "active" : ""}`} 
+                            to={tab.to}>
+                            {tab.label}
+                        </Link>
                     </li>
                 );
             })}
@@ -19,9 +32,9 @@ const PluginPageTabs = ({tabs}) => {
 PluginPageTabs.displayName = 'PluginPageTabs';
 PluginPageTabs.propTypes = {
     tabs: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        to: PropTypes.string.isRequired,
-        label: PropTypes.string.isRequired,
+            id: PropTypes.string.isRequired,
+            to: PropTypes.string.isRequired,
+            label: PropTypes.string.isRequired,
     }))
 };
 
