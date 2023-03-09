f0f97f5f6f2d2a4c190038cf91f8410ef9b94b3a
Gavin Mogan <git@gavinmogan.com>
2023-03-09 15:07:48 -0800

    more fixes toward standarizing everything with slashes

--- plugins/plugin-site/gatsby-browser.js
+++ plugins/plugin-site/gatsby-browser.js
@@ -1,12 +1,12 @@
 exports.onClientEntry = function () {
     if (window.location.hash === '#releases') {
-        window.location.href = `/${window.location.pathname.split('/')[1]}/releases`;
+        window.location.href = `/${window.location.pathname.split('/')[1]}/releases/`;
     }
     else if (window.location.hash === '#issues') {
-        window.location.href = `/${window.location.pathname.split('/')[1]}/issues`;
+        window.location.href = `/${window.location.pathname.split('/')[1]}/issues/`;
     }
     else if (window.location.hash === '#dependencies') {
-        window.location.href = `/${window.location.pathname.split('/')[1]}/dependencies`;
+        window.location.href = `/${window.location.pathname.split('/')[1]}/dependencies/`;
     }
 
     require.ensure(['@sentry/browser'], (require) => {
