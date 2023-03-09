88eb91dd57a45e084a7dd8fbbbfb0992438883ce
Harsh <harshgupta3341@gmail.com>
2023-03-10 02:32:48 +0530

    minor change

--- plugins/plugin-site/src/components/PluginPageTabs.jsx
+++ plugins/plugin-site/src/components/PluginPageTabs.jsx
@@ -3,26 +3,13 @@ import PropTypes from 'prop-types';
 
 import {Link} from 'gatsby';
 
-const PluginPageTabs = ({ tabs }) => {
-
-    let isAnyTabActive = false;
-    tabs.forEach((tab) => {
-        if (window.location.pathname === tab.to) {
-            isAnyTabActive = true;
-        }
-    });
-
+const PluginPageTabs = ({tabs}) => {
     return (
         <ul className="nav nav-pills">
-            {tabs.map((tab, index) => {
+            {tabs.map(tab => {
                 return (
                     <li className="nav-item" key={tab.id}>
-                        <Link
-                            activeClassName="active"
-                            className={`nav-link ${!isAnyTabActive && index === 0 ? "active" : ""}`} 
-                            to={tab.to}>
-                            {tab.label}
-                        </Link>
+                        <Link activeClassName="active" className="nav-link" to={tab.to}>{tab.label}</Link>
                     </li>
                 );
             })}
@@ -32,9 +19,9 @@ const PluginPageTabs = ({ tabs }) => {
 PluginPageTabs.displayName = 'PluginPageTabs';
 PluginPageTabs.propTypes = {
     tabs: PropTypes.arrayOf(PropTypes.shape({
-            id: PropTypes.string.isRequired,
-            to: PropTypes.string.isRequired,
-            label: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired,
+        to: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
     }))
 };
 
