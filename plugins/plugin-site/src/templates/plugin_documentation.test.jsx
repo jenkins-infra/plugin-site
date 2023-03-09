d3239c0639e2c25fa05e5a292eb7dcafa70bbda1
Gavin Mogan <git@gavinmogan.com>
2023-03-08 23:29:10 -0800

    make sure we have very very basic test scaffolding for all the new pages

---
+++ plugins/plugin-site/src/templates/plugin_documentation.test.jsx
@@ -0,0 +1,31 @@
+
+import React from 'react';
+
+import {render} from '@testing-library/react';
+
+import TemplatePluginDocumentation from './plugin_documentation';
+
+describe('component - TemplatePluginDocumentation', () => {
+    it('renders', async () => {
+        const plugin = {
+            name: 'fake',
+            title: 'fake plugin',
+            buildDate: '0',
+            labels: [],
+            developers: [],
+            stats: {
+                installations: [],
+                currentInstalls: 15
+            },
+            gav: 'something',
+            dependencies: [],
+            wiki: {
+                url: 'https://somewhere.com',
+                content: 'something'
+            }
+        };
+        const {container} = render(<TemplatePluginDocumentation data={{jenkinsPlugin: plugin}} />);
+
+        expect(container).toBeTruthy();
+    });
+});
