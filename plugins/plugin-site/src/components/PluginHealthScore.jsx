1708e13db67ad60f0fc4b6fd9a0510b3be7fdd47
Harsh <harshgupta3341@gmail.com>
2023-03-24 02:37:26 +0530

    added plugin-health-score in search page

---
+++ plugins/plugin-site/src/components/PluginHealthScore.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Progress } from 'antd';
+
+function PluginHealthScore({healthScore,color}) {
+    return (
+        <>
+        <h6>Health Score</h6>
+        <Progress percent={healthScore} strokeColor={color} />
+        </>
+    );
+}
+
+PluginHealthScore.propTypes = {
+    healthScore: PropTypes.number.isRequired
+};
+
+export default PluginHealthScore;
