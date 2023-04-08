3773c2916404328fb1277154a07034819e468efb
Harsh <harshgupta3341@gmail.com>
2023-04-09 01:54:00 +0530

    Fixed alignment

--- plugins/plugin-site/src/utils/algolia-queries.js
+++ plugins/plugin-site/src/utils/algolia-queries.js
@@ -13,14 +13,14 @@ function pluginQueries() {
               trend
             }
             healthScore {
+              value
+              version
+              details {
+                key
                 value
-                    version
-                    details {
-                        key
-                        value
-                        coefficient
-                    }
-                    timestamp
+                coefficient
+              }
+              timestamp
             }
             developers {
               name
@@ -33,7 +33,7 @@ function pluginQueries() {
       }
     }
     `;
-    function pageToAlgoliaRecord({node: {id, ...rest}}) {
+    function pageToAlgoliaRecord({ node: { id, ...rest } }) {
         return {
             objectID: id,
             slug: `/${id.trim()}/`,
@@ -42,50 +42,40 @@ function pluginQueries() {
     }
     return {
         query: pageQuery,
-        transformer: ({data}) => data.pages.edges.map(pageToAlgoliaRecord),
-        indexName: 'Plugins',
+        transformer: ({ data }) => data.pages.edges.map(pageToAlgoliaRecord),
+        indexName: "Plugins",
         synonyms: [
             {
-                type: 'synonym',
-                synonyms: ['perforce', 'p4'],
-                objectID: 'syn-1617250859718-18'
-            }
+                type: "synonym",
+                synonyms: ["perforce", "p4"],
+                objectID: "syn-1617250859718-18",
+            },
         ],
         settings: {
             paginationLimitedTo: 2000, // they recommend 1000, to keep speed up and prevent people from scraping, but both are fine to us
-            optionalWords: [
-                'jenkins',
-                'plugin',
-                'plugins'
-            ],
+            optionalWords: ["jenkins", "plugin", "plugins"],
             ranking: [
-                'typo',
-                'geo',
-                'words',
-                'filters',
-                'proximity',
-                'attribute',
-                'exact',
-                'custom'
+                "typo",
+                "geo",
+                "words",
+                "filters",
+                "proximity",
+                "attribute",
+                "exact",
+                "custom",
             ],
-            customRanking: [
-                'desc(stats.currentInstalls)'
-            ],
-            attributesForFaceting: [
-                'labels'
-            ],
+            customRanking: ["desc(stats.currentInstalls)"],
+            attributesForFaceting: ["labels"],
             attributesToIndex: [
-                'name',
-                'title',
-                'developers.name',
-                'developers.id',
-                'excerpt'
+                "name",
+                "title",
+                "developers.name",
+                "developers.id",
+                "excerpt",
             ],
         },
     };
 }
 
-const queries = [
-    pluginQueries(),
-];
+const queries = [pluginQueries()];
 module.exports = queries;
