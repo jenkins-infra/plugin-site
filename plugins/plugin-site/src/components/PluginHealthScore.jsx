1cf152ee0dc5f27c4e5700faa585b55bbec1c57e
Harsh <harshgupta3341@gmail.com>
2023-04-13 02:18:30 +0530

    added info link for Plugin Health Score

--- plugins/plugin-site/src/components/PluginHealthScore.jsx
+++ plugins/plugin-site/src/components/PluginHealthScore.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {Progress} from 'reactstrap';
+import {Progress, Tooltip} from 'reactstrap';
 
-function PluginHealthScore({healthScore}) {
+function PluginHealthScore({healthScore, name}) {
     const score = healthScore.value || 0;
     const color =
     score > 80 ? 'success' : score > 60 ? 'warning' : 'danger';
+
+    const tooltipId = `tooltip-${name}`;
+    const [tooltipOpen, setTooltipOpen] = useState(false);
+    const toggle = () => setTooltipOpen(!tooltipOpen);
     return (
         <>
             <div>
-                Health Score
+                <div>
+                    Health Score
+                    <span
+                        id={tooltipId}
+                        style={{marginLeft: '5px',
+                            color: '#0D6EFD'}}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            setTooltipOpen(true);
+                        }}
+                    >
+                        ?
+                    </span>
+                    <Tooltip
+                        placement="top"
+                        isOpen={tooltipOpen}
+                        autohide={false}
+                        target={tooltipId}
+                        toggle={toggle}
+                    >
+                        View the details about plugin&apos;s
+                        {' '}
+                        <a
+                            href={`https://plugin-health.jenkins.io/scores/${name}`}
+                        >
+                            health score
+                        </a>
+                    </Tooltip>
+                </div>
                 <div>
                     {score}
                     /100
@@ -24,6 +56,7 @@ PluginHealthScore.propTypes = {
     healthScore: PropTypes.shape({
         value: PropTypes.number,
     }),
+    name: PropTypes.string,
 };
 
 export default PluginHealthScore;
