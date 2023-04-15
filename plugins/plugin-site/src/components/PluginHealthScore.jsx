8fb11c7b20ef6333f71999ab97e398444cd4238d
Harsh Gupta <101264150+Harsh3341@users.noreply.github.com>
2023-04-15 09:08:55 +0530

    added info link for Plugin Health Score (#1331)
    Co-authored-by: Gavin Mogan <github@gavinmogan.com>

--- plugins/plugin-site/src/components/PluginHealthScore.jsx
+++ plugins/plugin-site/src/components/PluginHealthScore.jsx
@@ -1,15 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {Progress} from 'reactstrap';
+import {Progress, Tooltip} from 'reactstrap';
 
-function PluginHealthScore({healthScore}) {
+function PluginHealthScore({healthScore, name}) {
     const score = healthScore.value || 0;
     const color =
     score > 80 ? 'success' : score > 60 ? 'warning' : 'danger';
+
+    const tooltipId = `tooltip-${name}`;
+    const [tooltipOpen, setTooltipOpen] = useState(false);
+    const toggle = () => setTooltipOpen(!tooltipOpen);
     return (
         <>
             <div>
-                Health Score
+                <div>
+                    Health Score
+                    <a
+                        href={`https://plugin-health.jenkins.io/scores/${name}`}
+                        id={tooltipId}
+                        style={{marginLeft: '5px'}}
+                        onClick={(e) => {
+                            e.stopPropagation();
+                            setTooltipOpen(!tooltipOpen);
+                        }}
+                    >
+                        ?
+                    </a>
+                    <Tooltip
+                        placement="top"
+                        isOpen={tooltipOpen}
+                        autohide={false}
+                        target={tooltipId}
+                        toggle={toggle}
+                        onClick={(e) => {e.stopPropagation();}}
+                    >
+                        View the details about plugin&apos;s
+                        {' '}
+                        <a
+                            href={`https://plugin-health.jenkins.io/scores/${name}`}
+                        >
+                            health score
+                        </a>
+                    </Tooltip>
+                </div>
                 <div>
                     {score}
                     /100
@@ -24,6 +57,7 @@ PluginHealthScore.propTypes = {
     healthScore: PropTypes.shape({
         value: PropTypes.number,
     }),
+    name: PropTypes.string,
 };
 
 export default PluginHealthScore;
