bf6635f93cd41f804758445030f9a73b9eeac81b
Adrien Lecharpentier <alecharpentier@cloudbees.com>
2023-06-29 11:47:10 +0200

    Displays raw data from plugin health

--- plugins/plugin-site/src/components/PluginHealthScore.jsx
+++ plugins/plugin-site/src/components/PluginHealthScore.jsx
@@ -1,18 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function PluginHealthScore(healthScore) {
+function ScoreDetails({data: {name, components, value, weight, description}}) {
     return (
-        <div id="pluginHealthScore--container" className="container">
-            <pre>{JSON.stringify(healthScore, null, 2)}</pre>
+        <div className="container">
+            <h3>{name}</h3>
+            <pre>{JSON.stringify({components, value, weight, description}, null, 2)}</pre>
         </div>
     );
 }
-
-PluginHealthScore.propTypes = {
-    healthScore: {
+ScoreDetails.propTypes = {
+    data: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        components: PropTypes.arrayOf(PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            value: PropTypes.number.isRequired,
+            max: PropTypes.number.isRequired,
+            description: PropTypes.string.isRequired,
+        })),
         value: PropTypes.number.isRequired,
-        details: PropTypes.shape({}).isRequired
-    }
+        weight: PropTypes.number.isRequired,
+        description: PropTypes.string.isRequired,
+    })
+};
+
+function PluginHealthScore({healthScore: {value: score, details}}) {
+    return (
+        <div id="pluginHealthScore--container" className="container">
+            <div>{score}</div>
+            <div>
+                {details.sort((d1, d2) => d1.name - d2.name).map((data, idx) => {
+                    return (
+                        <ScoreDetails key={idx} data={data} />
+                    );
+                })}
+            </div>
+        </div>
+    );
+}
+PluginHealthScore.propTypes = {
+    healthScore: PropTypes.shape({
+        value: PropTypes.number.isRequired,
+        details: PropTypes.arrayOf(
+            ScoreDetails.propTypes.data
+        ).isRequired
+    }).isRequired
 };
 export default PluginHealthScore;
