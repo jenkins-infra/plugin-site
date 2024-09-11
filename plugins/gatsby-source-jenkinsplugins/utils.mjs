89c4d7d7e876b7cd4b248a5fd4f46dc6991c8f37
Damien Duportal <damien.duportal@gmail.com>
2024-09-11 21:24:49 +0200

    Update utils.mjs

--- plugins/gatsby-source-jenkinsplugins/utils.mjs
+++ plugins/gatsby-source-jenkinsplugins/utils.mjs
@@ -30,7 +30,7 @@ const requestGET = async ({url, reporter}) => {
 
     try {
         const results = await axios.get(url);
-        if (results.status !== 200) {
+        if (results.status >= 400) {
             throw results.data;
         }
         return results.data;
